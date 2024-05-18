e8b6bab2638fe535ff0b9683473d279e0e72db20
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-05-19 00:39:26 +0200

    add rdpcap

--- test/read.py
+++ test/read.py
@@ -1,7 +1,8 @@
 import pyshark, sys
-from scapy.all import Dot11ProbeReq
+from scapy.all import Dot11ProbeReq, rdpcap
 
-capture = pyshark.FileCapture(sys.argv[1])
+# capture = pyshark.FileCapture(sys.argv[1])
+capture = rdpcap(sys.argv[1])
 
 probe = [pkt for pkt in capture if Dot11ProbeReq in pkt]
 
