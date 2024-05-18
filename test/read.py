83400af09fea0a0122043035ddc8f74f612a8329
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-05-19 00:53:31 +0200

    filter by string

--- test/read.py
+++ test/read.py
@@ -4,7 +4,8 @@ from scapy.all import Dot11ProbeReq, rdpcap
 # capture = pyshark.FileCapture(sys.argv[1])
 capture = rdpcap(sys.argv[1])
 
-probe = [pkt for pkt in capture if Dot11ProbeReq in pkt]
+# probe = [pkt for pkt in capture if Dot11ProbeReq in pkt]
+probe = [pkt for pkt in capture if 'Wildcard' in str(pkt)]
 
 for packet in probe:
     print(packet)
